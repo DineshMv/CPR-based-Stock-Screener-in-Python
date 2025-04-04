dffeb116f56333ab08d54b5e6b862103ed0bda34
Dinesh Mv <DineshMv@users.noreply.github.com>
2025-04-04 16:09:48 -0400

    replaced .download property
    Signed-off-by: Dinesh Mv <DineshMv@users.noreply.github.com>

--- com/dino/stocktracker/Dataset/get_dataset.py
+++ com/dino/stocktracker/Dataset/get_dataset.py
@@ -3,47 +3,64 @@ import pathlib
 import pandas as pd
 import yfinance as yf
 
-from config import dataset_path
+from config import dataset_path, all_mcap_stocks
 
 # List of Medium to Mega Market Capital stock in NASDAQ, NYSE:
 ticker_strings = []
-stock_tracker_ticker = all_mcap_stocks
+mega_tickers = all_mcap_stocks
 
 
-# stock_tracker_ticker = ['AAPL', 'AMZN', 'GOOG', 'TSLA', 'MSFT']
+# stock_tracker_ticker = ['AAPL', 'AMZN', 'GOOG', 'TSLA', 'MSFT', 'XOM', 'JNJ']
 
-
-def fetch_from_yfinance(user_choice, from_date, to_date):
+def fetch_from_yfinance(stock_list, time_period):
     df_list = list()
-    for ticker in user_choice:
-        data = yf.download(ticker, group_by="Ticker", start=from_date, end=to_date)
-        data['Ticker'] = ticker  # added this column because the dataframe doesn't contain a column with the ticker
-        df_list.append(data)
-    print(f'Shelbot\U0001F60E: Stock Data extraction from YFinance is completed...')
+    # Fetch and clean data
+    for stock in stock_list:
+        stock_selected = yf.Ticker(stock)
+        print(f'Shelbot\U0001F60E: Scrapping historical data for:', stock)
+        data = (
+            stock_selected.history(period=time_period)
+            .drop(columns=['Dividends', 'Stock Splits'])
+            .round(2)
+            .assign(Volume=lambda x: x['Volume'].astype(int))
+            .reset_index()
+        )
 
-    # Prepare data frame by passing the list using pandas
-    df = pd.concat(df_list)
-    print(f'Shelbot\U0001F60E: Preparing the dataframe...')
+        # Add Ticker Name in column-1
+        data['Ticker'] = stock
+
+        # Convert Date format to MM/DD/YYYY
+        data['Date'] = data['Date'].dt.strftime('%m/%d/%Y')
+
+        # Reorder columns
+        data = data[['Ticker', 'Date', 'Open', 'High', 'Low', 'Close', 'Volume']]
+        df_list.append(data)
+
+        # Reset index
+        df = pd.concat(df_list, ignore_index=False)
+        df = df.set_index(['Ticker', 'Date'])
+
     # save to csv
     df.to_csv(dataset_path + 'stock_data.csv')
+    print(f"Shelbot\U0001F60E: Dataset 'stock_data.csv' is generated!")
 
 
-def fetch_tickers(from_date, to_date):
+def fetch_tickers(time_period):
     user_input = int(input("Shelbot\U0001F60E: Do you want to create stock tracker with list of USA Medium - Mega "
                            "MCAP stocks: \n 1. 'Yes' or 2. 'No' \n"))
 
     if user_input == 1:
-        print(f'Shelbot\U0001F60E: Fetching the stock data using yfinance library for:\n ', stock_tracker_ticker)
-        fetch_from_yfinance(stock_tracker_ticker, from_date, to_date)
+        print(f'Shelbot\U0001F60E: Fetching the stock data using yfinance library for:\n ', mega_tickers)
+        fetch_from_yfinance(mega_tickers, time_period)
         print(f"Shelbot\U0001F60E: Stock Data is downloaded to file 'stock_data.csv'...!")
 
     elif user_input == 2:
         user_inp2 = input("Shelbot\U0001F60E: Provide the list of stocks you want to create a tracker: \n")
         new_stocks_list = user_inp2.split()
         ticker_strings.extend(new_stocks_list)
-        print(f'Shelbot\U0001F60E: Fetching the stock data using yfinance library for: ', ticker_strings)
-        fetch_from_yfinance(ticker_strings, from_date, to_date)
-        print(f"Shelbot\U0001F60E: Stock Data is downloaded to file 'stock_data.csv'...!")
+        print(f'Shelbot\U0001F60E: List of stocks selected:', ticker_strings)
+        fetch_from_yfinance(ticker_strings, time_period)
+        # print(f"Shelbot\U0001F60E: Stock Data is downloaded to file 'stock_data.csv'...!")
 
     else:
         if not 1 or 2:
@@ -51,21 +68,20 @@ def fetch_tickers(from_date, to_date):
     print(f'________________________________________________________________________________')
 
 
-def check_initial_dataset(from_date, to_date):
+def check_initial_dataset(time_period):
     file = pathlib.Path(dataset_path + "stock_data.csv")
-
     if file.exists():
         user_in = int(input(f"Shelbot\U0001F60E: Do you want to use existing dataset? \n 1. 'Yes' or 2. 'No'\n"))
         if user_in == 1:
             print(f"Shelbot\U0001F60E: Existing Dataset - 'stock_data.csv' is selected")
         elif user_in == 2:
-            fetch_tickers(from_date, to_date)
+            fetch_tickers(time_period)
     else:
-        fetch_tickers(from_date, to_date)
+        fetch_tickers(time_period)
     print(f'________________________________________________________________________________')
 
 
-def check_pivot_dataset(from_date, to_date):
+def check_pivot_dataset(time_period):
     from com.dino.stocktracker.Find_pivots.calculate_pivots import calculate_pivots
 
     file = pathlib.Path(dataset_path + 'stock_data.csv')
@@ -75,13 +91,13 @@ def check_pivot_dataset(from_date, to_date):
             input(f"Shelbot\U0001F60E: Do you want to calculate pivots for stock data? \n 1. 'Yes'  or 2. 'No' \n"))
 
         if user_inp1 == 1:
-            calculate_pivots(from_date, to_date)
+            calculate_pivots(time_period)
         elif user_inp1 == 2:
             print(f'Shelbot\U0001F60E: You chose not to calculate pivots')
     else:
         print(f"Shelbot\U0001F60E: Please generate OHLC data with your preferred list of stocks!")
     print(f'________________________________________________________________________________')
 
-# check_initial_dataset()
-#
-# check_pivot_dataset()
+
+check_initial_dataset("1mo")
+# check_pivot_dataset('1mo')
